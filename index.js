9dd1126fbf136e108b62571fc62f98ee0ac7a187
WarioNeila86 <mariogar1001@gmail.com>
2020-10-24 22:45:42 +0200

    Express app creted with some simple handlers for some get requests

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// handle get request to our server
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(JSON.stringify([1, 2, 3]));
+});
+
+// make our app listen at a given port
+app.listen(3000, () => {
+    console.log('Listening on 3000...');
+});
