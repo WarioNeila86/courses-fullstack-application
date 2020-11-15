7325939caebca7dc14eb984bef140bb23e2ff1a6
WarioNeila86 <mariogar1001@gmail.com>
2020-11-15 13:26:42 +0100

    Courses are now stored in/read from a DB instead of using a local array

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const app = express();
 // recognize the incoming request object as a JSON Object - this method is called as a middleware
 app.use(express.json());
 
-// list of courses
-// TODO: move this into a DB in the future
-const courses = [
-    {id: 1, name: 'Course 1'},
-    {id: 2, name: 'Course 2'},
-    {id: 3, name: 'Course 3'}
-];
+const database = require('./api/server/src/models');
 
 // GET request for the root path of the server will show a Hello World message
 app.get('/', (req, res) => {
@@ -19,74 +13,89 @@ app.get('/', (req, res) => {
 });
 
 // retrieve all courses
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
+app.get('/api/courses', async (req, res) => {
+    // res.send(courses);
+    await database.Course.findAll().then(books => {
+        res.send(books);
+    }).catch(error => {
+        res.status(404).send(error.message);
+    });
 });
 
 // retrieve a specific course by providing its id in url path
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(course => course.id === parseInt(req.params.id, 10));
-    if (!course) {
+app.get('/api/courses/:id', async (req, res) => {
+    const foundCourse = await database.Course.findOne({
+        where: { id: Number(req.params.id) }
+    });
+    if (!foundCourse) {
         res.status(404).send(`The course with id '${req.params.id}' was not found`);
     } else {
-        res.send(course);
+        res.send(foundCourse);
     }
 });
 
 // create a new course providing its name in request body
-app.post('/api/courses', (req, res) => {
-    const {error: validationError} = validateCourse(req.body);
+app.post('/api/courses', async (req, res) => {
+    const newCourse = req.body;
 
+    // validate, if invalid, return 400 / Bad Request
+    const {error: validationError} = validateCourse(newCourse);
     if (validationError) {
         const {message} = validationError;
         return res.status(400).send(`Wrong format: ${message}`);
     }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
+    const addedCourse = await database.Course.create(newCourse);
+    if (addedCourse) {
+        res.send(addedCourse);
+    } else {
+        console.log('Unable to add course');
+    }
 });
 
 // update an existing course by providing its id in url path and new name in body
-app.put('/api/courses/:id', (req, res) => {
-    // look for the course
-    // if course does not exist, return 404 / Not Found
-    const course = courses.find(course => course.id === parseInt(req.params.id, 10));
-    if (!course) {
-        return res.status(404).send(`The course with id '${req.params.id}' was not found`);
-    }
+app.put('/api/courses/:id', async (req, res) => {
+    const newData = req.body;
 
     // validate, if invalid, return 400 / Bad Request
-    const {error: validationError} = validateCourse(req.body);
-
+    const {error: validationError} = validateCourse(newData);
     if (validationError) {
         const {message} = validationError;
         return res.status(400).send(`Wrong format: ${message}`);
     }
 
-    // Return updated course
-    course.name = req.body.name;
-    res.send(course);
+    // search for the course, if course does not exist return 404 / Not Found
+    const foundCourse = await database.Course.findOne({
+        where: { id: Number(req.params.id) }
+    });
+    if (!foundCourse) {
+        return res.status(404).send(`The course with id '${req.params.id}' was not found`);
+    } else {
+        // if found, replace the name and save the changes -- this will perform the UPDATE in DB
+        foundCourse.name = newData.name;
+        await foundCourse.save();
+        res.send(foundCourse);
+    }
 });
 
 // delete a course by providing its id in url path
-app.delete('/api/courses/:id', (req, res) => {
-    // look for the course
-    // if course does not exist, return 404 / Not Found
-    const course = courses.find(course => course.id === parseInt(req.params.id, 10));
-    if (!course) {
-        return res.status(404).send(`The course with id '${req.params.id}' was not found`);
+app.delete('/api/courses/:id', async (req, res) => {
+    // search for the course, if course does not exist return 404 / Not Found
+    const foundCourse = await database.Course.findOne({
+        where: { id: Number(req.params.id) }
+    });
+    if (!foundCourse) {
+        res.status(404).send(`The course with id '${req.params.id}' was not found`);
+    } else {
+        // if found, delete the course
+        await database.Course.destroy({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+
+        // return deleted course -- sequelize destroy returns the number of deleted rows, not the rows itself
+        res.send(foundCourse);
     }
-
-    // delete the course
-    const courseIndex = courses.indexOf(course);
-    courses.splice(courseIndex, 1);
-
-    // return deleted course
-    res.send(course);
 });
 
 // read PORT from env variable PORT or use port 3000 as default
