8ea1fdabb14455b53326675d02c4f5169dddecdb
WarioNeila86 <mariogar1001@gmail.com>
2020-11-28 01:03:46 +0100

    eslint added and executed on pre-commit hook

--- api/server/src/seeders/20201114135040-demo-course.js
+++ api/server/src/seeders/20201114135040-demo-course.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: (queryInterface) => {
     return queryInterface.bulkInsert('Courses', [
       {
         name: 'Demo course 1',
@@ -15,7 +15,7 @@ module.exports = {
       }
     ]);
   },
-  down: (queryInterface, Sequelize) => {
+  down: (queryInterface) => {
     return queryInterface.bulkDelete('Courses', null, {});
   }
 };
