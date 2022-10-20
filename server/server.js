97c2e3c49dae0290952c4b078647efcffca0e5cc
WarioNeila86 <6084473+WarioNeila86@users.noreply.github.com>
2022-10-20 22:44:32 +0200

    change default port number for server to 4000

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const {logger} = require('./logger');
 
 // read PORT from env variable PORT or use port 3000 as default
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // start the server listening at a given port
 app.listen(port, () => logger.info(`Listening on ${port}...`));
