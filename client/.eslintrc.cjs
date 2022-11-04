b90e96b6a904b13ec5ed387df9c681febb78df03
WarioNeila86 <6084473+WarioNeila86@users.noreply.github.com>
2022-11-04 22:49:27 +0100

    prettier added to React project

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,27 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'react/prop-types': 'off'
-  }
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: ['plugin:react/recommended', 'standard', 'eslint-config-prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/prop-types': 'off',
+    },
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,52 +5,48 @@ import './App.css'
 
 import { getAllCourses, getCourseById } from './services/courses-api'
 
-export function App () {
-  const [courses, setCourses] = useState(null)
-  const [search, setSearch] = useState('')
-  const [course, setCourse] = useState(null)
+export function App() {
+    const [courses, setCourses] = useState(null)
+    const [search, setSearch] = useState('')
+    const [course, setCourse] = useState(null)
 
-  useEffect(() => {
-    getAllCourses().then(setCourses).catch(console.error)
-  }, [])
+    useEffect(() => {
+        getAllCourses().then(setCourses).catch(console.error)
+    }, [])
 
-  return (
-    <div className="container">
-      <h2>Find Course</h2>
-      <form onSubmit={ev => {
-        ev.preventDefault()
-        console.log(search)
-        getCourseById(search).then(setCourse).catch(console.error)
-      }}>
-        <input
-          type="text"
-          name="search"
-          autoComplete="off"
-          value={search}
-          onChange={ev => setSearch(ev.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-      {
-        !course
-          ? (<h3>Enter an id in the previous form to search for a course</h3>)
-          : (
-            <div>
-              <h3>Searching course with id: {search}</h3>
-              <Course course={course} />
-            </div>
-            )
-      }
-      <h2>Courses List</h2>
-      {
-        !courses || courses.length === 0
-          ? (
-            <p>Loading...</p>
-            )
-          : (
-            <CourseList courses={courses} />
-            )
-      }
-    </div>
-  )
+    return (
+        <div className="container">
+            <h2>Find Course</h2>
+            <form
+                onSubmit={(ev) => {
+                    ev.preventDefault()
+                    console.log(search)
+                    getCourseById(search).then(setCourse).catch(console.error)
+                }}
+            >
+                <input
+                    type="text"
+                    name="search"
+                    autoComplete="off"
+                    value={search}
+                    onChange={(ev) => setSearch(ev.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+            {!course ? (
+                <h3>Enter an id in the previous form to search for a course</h3>
+            ) : (
+                <div>
+                    <h3>Searching course with id: {search}</h3>
+                    <Course course={course} />
+                </div>
+            )}
+            <h2>Courses List</h2>
+            {!courses || courses.length === 0 ? (
+                <p>Loading...</p>
+            ) : (
+                <CourseList courses={courses} />
+            )}
+        </div>
+    )
 }
