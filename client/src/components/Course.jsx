5bde821855b168472136b32af202359ce84dea03
WarioNeila86 <6084473+WarioNeila86@users.noreply.github.com>
2023-06-22 23:04:03 +0200

    apply Material UI to Course and CourseList components

--- client/src/components/Course.jsx
+++ client/src/components/Course.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import './Course.css'
+import {
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemText
+} from '@mui/material'
+import { Info } from '@mui/icons-material'
 
 export function Course({ course }) {
   const { name, id } = course
 
   return (
-    <div className="course">
-      <div>{id}</div> <div>{name}</div>
-      <div className="link">
-        <a href={`/courseDetails/${id}`}>More details</a>
-      </div>
-    </div>
+    <ListItem disablePadding>
+      <ListItemButton component="a" href={`/courseDetails/${id}`}>
+        <ListItemText primary={`${id} - ${name}`} />
+        <IconButton aria-label="show more details" size="small">
+          <Info fontSize="inherit" />
+        </IconButton>
+      </ListItemButton>
+    </ListItem>
   )
 }
--- client/src/components/CourseList.jsx
+++ client/src/components/CourseList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { Box, List, Typography } from '@mui/material'
 import { Course } from './Course'
 import { AddCourse } from './AddCourse'
-import './CourseList.css'
 
 import { getAllCourses } from '../services/courses-api'
 
@@ -13,16 +13,29 @@ export function CourseList() {
   }, [])
 
   return (
-    <div className="courseList">
-      <h1>Courses List</h1>
+    <Box
+      sx={{
+        border: '1px solid black',
+        padding: '1rem',
+        margin: '1rem 0',
+        'border-radius': '0.5rem'
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        Courses list
+      </Typography>
       {!courses || courses.length === 0 ? (
         <p>Loading...</p>
       ) : (
-        courses
-          .sort((a, b) => a.id - b.id)
-          .map((course) => <Course course={course} key={course.id} />)
+        <List sx={{ width: '100%', maxWidth: 360 }}>
+          {courses
+            .sort((a, b) => a.id - b.id)
+            .map((course) => (
+              <Course course={course} key={course.id} />
+            ))}
+        </List>
       )}
       <AddCourse courses={courses} setCourses={setCourses} />
-    </div>
+    </Box>
   )
 }
