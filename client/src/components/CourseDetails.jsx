863059a384b609f1b0effef963188bcb983f6e5d
WarioNeila86 <6084473+WarioNeila86@users.noreply.github.com>
2024-01-18 19:01:14 +0100

    add formatting to CourseDetails view

--- client/src/components/CourseDetails.jsx
+++ client/src/components/CourseDetails.jsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography
+} from '@mui/material'
+import EditCalendarIcon from '@mui/icons-material/EditCalendar'
+import NumbersIcon from '@mui/icons-material/Numbers'
+import TitleIcon from '@mui/icons-material/Title'
+import TodayIcon from '@mui/icons-material/Today'
 import { EditCourse } from './EditCourse'
 import { DeleteCourse } from './DeleteCourse'
 
@@ -16,20 +27,43 @@ export function CourseDetails() {
 
   return (
     <>
-      <h1>Course Details</h1>
+      <Typography component="h1" variant="h5">
+        Courses details
+      </Typography>
       {!courseDetails ? (
         <p>Course not found</p>
       ) : (
-        <ul>
-          <li>Id: {courseDetails.id}</li>
-          <li>Name: {courseDetails.name}</li>
-          <li>
-            Created on: {new Date(courseDetails.createdAt).toLocaleString()}
-          </li>
-          <li>
-            Updated on: {new Date(courseDetails.updatedAt).toLocaleString()}
-          </li>
-        </ul>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <NumbersIcon />
+            </ListItemIcon>
+            <ListItemText>{courseDetails.id}</ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <TitleIcon />
+            </ListItemIcon>
+            <ListItemText>{courseDetails.name}</ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <TodayIcon />
+            </ListItemIcon>
+            <ListItemText>
+              {new Date(courseDetails.createdAt).toDateString()}
+            </ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <EditCalendarIcon />
+            </ListItemIcon>
+            <ListItemText>
+              {new Date(courseDetails.updatedAt).toDateString()} at{' '}
+              {new Date(courseDetails.updatedAt).toTimeString()}
+            </ListItemText>
+          </ListItem>
+        </List>
       )}
       <EditCourse />
       <DeleteCourse />
